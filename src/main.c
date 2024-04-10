9836548ffac763df086aa7f948865f56f35849e7
meyer28j <jacob.meyer144@gmail.com>
2024-04-10 00:56:31 -0600

    implemented interrupt-based bpm change and calibration mode

--- src/main.c
+++ src/main.c
@@ -11,58 +11,99 @@ uint16_t mspb = 1000; // milliseconds per beat; used with delay_ms()
 
 void EXTI0_IRQHandler(void) {
 	
-	EXTI->PR |= EXTI_PR_PR0; // clear pending interrupt bit
-	
-	bpm += 1;
-	mspb = 60000 / bpm;
-	LD2_TOGGLE();
-	return;
-}
+	delay_ms(100); // simple handle for switch bounce
+		
+	// since this is a higher priority than EXTI1
+	// we can look to see if the EXTI1 pending bit
+	// is also set --> activate calibration mode
+	if ((EXTI->PR & EXTI_PR_PR1) == EXTI_PR_PR1) { // EXTI1 pending interrupt request
+		// other button is pressed too
+		EXTI->PR |= EXTI_PR_PR1; // clear pending request for other button
+		
+		/////////////////
+		// TODO: implement calibration mode entry
+		/////////////////
+		return;		
+	}
 
-
-void EXTI1_IRQHandler(void) {
-	
-	EXTI->PR |= EXTI_PR_PR1; // clear pending interrupt bit
-	
 	bpm -= 1;
 	mspb = 60000 / bpm;
 	LD2_TOGGLE();
+	
+	EXTI->PR |= EXTI_PR_PR0; // clear pending interrupt bit
+	
 	return;
 }
 
+void EXTI1_IRQHandler(void) {
+	
+	delay_ms(100); // simple handle for switch bounce
+
+	bpm += 1;
+	mspb = 60000 / bpm;
+	LD2_TOGGLE();
+	
+	EXTI->PR |= EXTI_PR_PR1; // clear pending interrupt bit
+
+	
+	return;
+}
+
+
+void EXTI9_5_IRQHandler(void) {
+	
+	delay_ms(100); // simple handle for switch bounce
+
+	// both buttons are pressed: enter calibration mode
+	if ((EXTI->PR & EXTI_PR_PR8) && (EXTI->PR & EXTI_PR_PR9)) {
+		/////////////////////////
+		// TODO: implement calibration mode
+		test_all(mspb / 8);
+		/////////////////////////
+		EXTI->PR = EXTI_PR_PR8;
+		EXTI->PR = EXTI_PR_PR9;
+		return;
+	}
+		
+	else if (EXTI->PR & EXTI_PR_PR8) { // interrupt caused by PA8
+		bpm -= 2;
+		mspb = 60000 / bpm;
+		EXTI->PR |= EXTI_PR_PR8; // clear pending interrupt bit	
+	} 
+	else if (EXTI->PR & EXTI_PR_PR9) { // interrupt cause by PA9
+		bpm += 2;
+		mspb = 60000 / bpm;
+		EXTI->PR |= EXTI_PR_PR9; // Clear the pending bit for PA9
+	}
+	return;
+}
 
 
 void initialize(void) {
 	// enable clock for ports A, B, C
 	RCC->APB2ENR |= (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN);
-	
-	
-	// enable on-board LED
-	enable_GPIO_output(GPIOA, 5);
-		
-	// enable on-board button
-	enable_GPIO_input(GPIOC, 13);
-	
-	// enable breadboard button input on PA8 and PA9
-	enable_GPIO_input(GPIOA, 8);
-	enable_GPIO_input(GPIOA, 9);
-	
-	///////////////////////// 
-	// ENABLE INTERRUPTS ON PORT B
-	// for breadboard buttons to change BPM
 	RCC->APB2ENR |= (RCC_APB2ENR_AFIOEN);	// enable clock for AFIO
+
+	enable_GPIO_output(GPIOA, 5);	// enable on-board LED
+	enable_GPIO_input(GPIOC, 13);	// enable on-board button
+
+	///////////////////////// 
+	// ENABLE INTERRUPTS ON PORT A
+	// for breadboard buttons to change BPM
 	
-	// Initialize EXTI0 registers for PB8
-	AFIO->EXTICR[0] |= AFIO_EXTICR1_EXTI0_PB; // select port B as source for EXTI0 events
-	EXTI->IMR |= EXTI_IMR_MR8; // unmask PB8 as the interrupt source
-	EXTI->FTSR |= EXTI_FTSR_TR8; // enable falling edge of signal on PB8 as the trigger
-	NVIC->ISER[0] |= NVIC_ISER_SETENA_6; // unmask EXTI0 as an interrupt source in the NVIC
 	
-	// Initialize EXTI1 registers for PB9
-	AFIO->EXTICR[1] |= AFIO_EXTICR1_EXTI1_PB; // select port B as source for EXTI1 events
-	EXTI->IMR |= EXTI_IMR_MR9; // unmask PB9 as the interrupt source
-	EXTI->FTSR |= EXTI_FTSR_TR9; // enable falling edge of signal on PB9 as the trigger
-	NVIC->ISER[1] |= NVIC_ISER_SETENA_6; // unmask EXTI1 as an interrupt source in the NVIC
+	// TEST PB0 as source
+	//AFIO->EXTICR[0] |= AFIO_EXTICR1_EXTI0_PB; // select port B as source for EXTI0 events
+	//EXTI->IMR |= EXTI_IMR_MR0; // unmask PB0 as the interrupt source
+	//EXTI->FTSR |= EXTI_FTSR_TR0; // enable falling edge of signal on PB0 as the trigger
+	//NVIC->ISER[0] |= NVIC_ISER_SETENA_6; // unmask EXTI0 as an interrupt source in the NVIC (using bit 6)
+	
+	// set PA8, PA9 as interrupt source using EXTI9_5
+	// EXTI9_5 is priority #23
+	AFIO->EXTICR[2] |= AFIO_EXTICR3_EXTI8_PA | AFIO_EXTICR3_EXTI9_PA; // select port A as source for EXTI9_5 events
+	EXTI->IMR |=  EXTI_IMR_MR8 | EXTI_IMR_MR9; // unmask PA8 and PA9 as the interrupt source
+	EXTI->FTSR |=  EXTI_FTSR_TR8 | EXTI_FTSR_TR9; // enable falling edge of signal on PA8 and PA9 as the trigger
+	NVIC->ISER[0] |= NVIC_ISER_SETENA_23; // unmask EXTI9_5 as an interrupt source in the NVIC
 	
 	bpm = 60;
 	mspb = 600;	
